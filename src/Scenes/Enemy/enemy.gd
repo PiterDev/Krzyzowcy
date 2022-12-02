b79ea9b18f3f412580c25a8765372fa1d0d4e964
Piotr Wilk <piter@pop-os.localdomain>
2022-12-02 20:39:50 +0100

    Setup Enemy Scene

--- src/Scenes/Enemy/enemy.gd
+++ src/Scenes/Enemy/enemy.gd
@@ -1,6 +1,11 @@
 extends KinematicBody2D
 
+onready var raycast := $RayCast2D as RayCast2D
 
 func _ready():
 	pass
 
+func _on_Area2D_body_entered(body: PhysicsBody2D):
+	var sees_player := false
+	
+	raycast.cast_to(body.position)
