39b913902012cc597ad436405779180161925598
tomasztomato <tomasztomato1@gmail.com>
2022-12-02 20:38:05 +0100

    Add basic shoot algorytm

--- src/Scenes/Player/player.gd
+++ src/Scenes/Player/player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export var walk_speed := 230.0
 var velocity := Vector2.ZERO
 var accel := 0.7
+var ammo := 3
 
 func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position()) # TODO: Smooth Rotation
@@ -16,3 +17,15 @@ func _process(_delta: float) -> void:
 	velocity = lerp(velocity, direction * walk_speed, accel)
 
 	velocity = move_and_slide(velocity)
+	
+	#Tomek tu był
+	if Input.is_action_just_pressed("mouse_left") and ammo > 0:
+		ammo -= 1
+		shoot()
+		
+func shoot():
+	print("shoot")
+	
+	
+	
+	
