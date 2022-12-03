10eb4b5bbff2b24e0cd319f644852de0308e2b22
tomasztomato <tomasztomato1@gmail.com>
2022-12-03 10:58:02 +0100

    Add about game

--- src/Scenes/Menu/play_button.gd
+++ src/Scenes/Menu/play_button.gd
@@ -6,3 +6,8 @@ extends Button
 func _on_Button_pressed() -> void:
 	get_tree().change_scene("res://Game.tscn")
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+
+func _on_Button3_pressed():
+	get_tree().change_scene("res://Scenes/Menu/About.tscn")
+	
