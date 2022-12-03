46fe8e273e53ebd13c6a61c66778683d63187e04
tomasztomato <tomasztomato1@gmail.com>
2022-12-03 07:32:10 +0100

    Add texture for three shells

--- src/Scenes/Player/HBoxContainer.gd
+++ src/Scenes/Player/HBoxContainer.gd
@@ -1,12 +1,17 @@
 extends HBoxContainer
 
 var used_bullet_texture = preload("res://Assets/usedBullet.png")
+var unused_bullet_texture = preload("res://Assets/unusedammo.png")
 
 onready var bullet_texturerect1 = get_node("TextureRect")
 onready var bullet_texturerect2 = get_node("TextureRect2")
 onready var bullet_texturerect3 = get_node("TextureRect3")
 
 func _process(delta):
+	if Globals.ammo == 3:
+		bullet_texturerect1.set_texture(unused_bullet_texture)
+		bullet_texturerect2.set_texture(unused_bullet_texture)
+		bullet_texturerect3.set_texture(unused_bullet_texture)
 	if Globals.ammo == 2:
 		bullet_texturerect1.set_texture(used_bullet_texture)
 	if Globals.ammo == 1:
